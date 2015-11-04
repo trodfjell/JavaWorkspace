e53beff1c3efe5c9f839e2816405f7e2072acb3c
trodfjell <trulsrodfjell@gmail.com>
2015-11-04 21:42:33 +0100

    Added StudentImporter and Student

---
+++ ComputerLottery/src/Student.java
@@ -0,0 +1,12 @@
+
+public class Student {
+	
+	public String name;
+	public int numberOfAttendances;
+	
+	
+	public Student(String name, int attendances){
+		this.name = name;
+		this.numberOfAttendances = attendances;
+	}
+}
---
+++ ComputerLottery/src/StudentImporter.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+
+public class StudentImporter {
+	
+	public static ArrayList<Student> importStudents(String filePath){
+		
+		ArrayList<Student> students = new ArrayList<Student>();
+
+		try {
+			
+			InputStream fis = new FileInputStream(filePath);
+			InputStreamReader isr = new InputStreamReader(fis, Charset.forName("ISO-8859-1"));
+			BufferedReader br = new BufferedReader(isr);
+			 
+			String line;
+			while ((line = br.readLine()) != null){
+				String[] studentCsv = line.split(",");
+				
+				Student student = new Student(studentCsv[0], Integer.parseInt(studentCsv[1]));
+				students.add(student);
+			}
+			
+			br.close();
+			isr.close();
+			fis.close();
+		}
+		
+		catch (FileNotFoundException e) {
+			System.out.println("Could not find file " + filePath);
+			System.exit(0);
+		}
+		catch (Exception fe){
+			System.out.println("Fatal exception on StudentImporter");
+			fe.printStackTrace();
+			System.exit(0);
+		}
+		
+		return students;
+		
+	}
+	
+}
